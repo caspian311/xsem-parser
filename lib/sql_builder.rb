de8d6a16c98e578832fda1689a48daaf18d49209
matt.todd <matt.c.todd@gmail.com>
2009-09-01 09:18:10 -0500

    created a sql builder to create the sql that will be inserted into the db
    	new file:   lib/sql_builder.rb
    	new file:   test/sql_builder_test.rb
    	modified:   test/test_helper.rb

---
+++ lib/sql_builder.rb
@@ -0,0 +1,25 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+class SqlBuilder
+  def initialize(books)
+    @books = books
+  end
+
+  def generate_sql
+    generated_sql = []
+    @books.each do |book|
+      book.chapters.each do |chapter|
+        chapter.verses.each do |verse|
+          sql = create_insert_statement(book, chapter, verse)
+          generated_sql << sql
+        end
+      end
+    end
+    return generated_sql
+  end
+
+  def create_insert_statement(book, chapter, verse)
+    ''
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,6 @@ $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 
 require File.dirname(__FILE__) + "/../lib/asv_parser"
 require File.dirname(__FILE__) + "/../lib/asv_stream_listener"
+require File.dirname(__FILE__) + "/../lib/sql_builder"
 
 require "test/unit"
