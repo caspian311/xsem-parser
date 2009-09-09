d52d1c8dcc694c0f61347005e8ef7b2132c9dcc2
matt.todd <matt.c.todd@gmail.com>
2009-09-09 10:47:11 -0500

    removed run.rb as it wasn't working anyway
    	deleted:    run.rb

--- run.rb
+++
@@ -1,9 +0,0 @@
-require "lib/sql_builder"
-require "lib/asv_parser"
-require "lib/asv_stream_listener"
-
-parser = AsvParser.new "data/asv-xsem.xml"
-sql_builder = SqlBuilder.new parser.books
-sql_builder.generate_sql.each do |statement|
-	puts statement
-end
