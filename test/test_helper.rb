89e6b4a24ba938776db2c638ffafe10187aecfdc
matt.todd <matt.c.todd@gmail.com>
2009-09-12 08:59:38 -0500

    added rubygems to tests to see if it would fix the build on my cruise box
    	modified:   test/test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,8 @@
 $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 
+require "rubygems"
+require "test/unit"
+
 require File.dirname(__FILE__) + "/../lib/bible"
 require File.dirname(__FILE__) + "/../lib/book"
 require File.dirname(__FILE__) + "/../lib/chapter"
@@ -9,5 +12,4 @@ require File.dirname(__FILE__) + "/../lib/xsem_parser"
 require File.dirname(__FILE__) + "/../lib/xsem_stream_listener"
 require File.dirname(__FILE__) + "/../lib/sql_builder"
 
-require "test/unit"
 require File.dirname(__FILE__) + "/../test/test_connection_info"
